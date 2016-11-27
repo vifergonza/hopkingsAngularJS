8afacf0b82bed038bfa93f52b9d7ea1b32fedeb2
vifergonza <vifergonza@gmail.com>
2016-11-27 01:05:08 +0100

    Module 5 solution

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,8 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://davids-restaurant.herokuapp.com')
+.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+//.constant('ApiPath', 'https://davids-restaurant.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
